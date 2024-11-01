d362f2ab7acb6eb247dc039d5849c8a3443bf7fc
Tounga-Saidou <23saiodu@gmail.com>
2024-11-01 13:39:15 +0200

    Nv-BR

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
-})
-export class NavbarComponent {
+}) export class NavbarComponent implements AfterViewInit {
+  private hamburger!: HTMLElement; // Using definite assignment assertion
+  private navLinks!: HTMLElement;
 
+  ngAfterViewInit(): void {
+      this.hamburger = document.getElementById('hamburger') as HTMLElement;
+      this.navLinks = document.getElementById('navLinks') as HTMLElement;
+
+      if (this.hamburger && this.navLinks) {
+          this.hamburger.addEventListener('click', this.toggleNav.bind(this));
+      }
+  }
+
+  private toggleNav(): void {
+      this.navLinks.classList.toggle('show');
+  }
 }
+
+
