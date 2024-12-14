96734ab35e732baa22c2fb734ff0fc0fae10ea8b
Tounga Saidou <23saidou@gmail.com.>
2024-12-14 14:53:10 +0200

    Carousel-slide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'SwissMint';
+
+  // Array to store the image URLs for the carousel
+  slides = [
+    'assets/homepage/street-wear.jpg',
+    'assets/homepage/high-end-cothing-brand.jpg',  
+    'assets/homepage/urban-wear.jpg'
+  ];
+
+  // The current index of the slide being displayed
+  currentIndex = 0;
+
+  // Method to move to the next slide
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+  }
+
+  // Method to move to the previous slide
+  prevSlide(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,27 +1,33 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
-}) export class NavbarComponent implements AfterViewInit {
-  private hamburger!: HTMLElement; // Using definite assignment assertion
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements AfterViewInit {
+  private hamburger!: HTMLElement;  // Using definite assignment assertion
   private navLinks!: HTMLElement;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   ngAfterViewInit(): void {
+    // Check if the platform is browser before accessing the DOM
+    if (isPlatformBrowser(this.platformId)) {
       this.hamburger = document.getElementById('hamburger') as HTMLElement;
       this.navLinks = document.getElementById('navLinks') as HTMLElement;
 
+      // Ensure the elements exist before adding the event listener
       if (this.hamburger && this.navLinks) {
           this.hamburger.addEventListener('click', this.toggleNav.bind(this));
       }
+    }
   }
 
   private toggleNav(): void {
-      this.navLinks.classList.toggle('show');
+    this.navLinks.classList.toggle('show');
   }
 }
-
-
